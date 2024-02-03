0dd84633073b8783532b09a93e2b34279ec668b5
Benjamin Hilderman <hild0033@algonquinlive.com>
2024-02-03 12:17:30 -0500

    sdf

--- src/components/StoreInfoLoader.js
+++ src/components/StoreInfoLoader.js
@@ -1,22 +1,21 @@
 //this is gonna load in the inventory of a store as well as any items that someone 
 //has selected
-import React from 'react';
+import React, { useEffect } from 'react';
 import './StoreInfoLoader.css';
 import { useQuery } from 'react-query';
 import readItemRequest from '../api/readItemRequest';
-import './StoreInfoLoader.css';
 import deleteItemRequest from '../api/deleteItemRequest';
 
 export default function StoreInfoLoader({ username }) {
-    const { isLoading, data: items } = useQuery(
-        ['items', username],
-        () => readItemRequest(username)
-    );
 
-    // ask if user wants to delete
+    // current date
+    const { isLoading, data: items } = useQuery(['items', username], () => readItemRequest(username));
+
+
+    // confirm if use wants to delete
     const handleDelete = async (itemId) => {
         if (window.confirm('Are you sure you want to delete this item?')) {
-            deleteItemRequest(itemId)
+            deleteItemRequest(itemId);
         }
     };
 
@@ -24,6 +23,7 @@ export default function StoreInfoLoader({ username }) {
         return <div>Loading...</div>;
     }
 
+    // inventory
     return (
         <div className="inventory-container">
             {items && items.length > 0 ? (
@@ -32,8 +32,10 @@ export default function StoreInfoLoader({ username }) {
                         <div className="item-info">
                             <p>Product: {item.product}</p>
                             <p>Expiration: {item.expiration}</p>
-                            <button className="delete-button" onClick={() => handleDelete(item._id)}>Delete</button></div>
-                        
+                        </div>
+                        <div>
+                            <button className="delete-button" onClick={() => handleDelete(item._id)}>Delete</button>
+                        </div>
                     </div>
                 ))
             ) : (
