b3d2044da63450b90ba52fa11ea0cb3382268da1
Saul Wolkove <saul.wolkove@gmail.com>
2024-02-03 10:54:53 -0500

    jun

--- src/api/deleteItemRequest.js
+++ src/api/deleteItemRequest.js
@@ -1,6 +1,5 @@
-export default (item)=>{
-    console.log(item._id)
-    return fetch(`http://localhost:3001/${item._id}`, {
+export default (id)=>{
+    return fetch(`http://localhost:3001/${id}`, {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json",
