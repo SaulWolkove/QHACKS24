f9737ebdf276d55752ed5bad390b5a5ad931fda3
Réna Hajjar <renashajjar@gmail.com>
2024-02-03 11:12:49 -0500

    bruh

--- src/utils/main.py
+++ src/utils/main.py
@@ -1,27 +1,57 @@
 import os;
-from langchain_community.llms import OpenAI;
+from langchain_community.llms import OpenAI
 from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
-import streamlit as st
+from langchain.chains import LLMChain, SequentialChain
 
-# callable
-load_dotenv()
-API_KEY = os.environ['OPENAI_API_KEY']
-print(API_KEY)
+def generateMealKit():
 
-llm = OpenAI(openai_api_key="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", temperature=0.3)
+    # callable
+    load_dotenv()
+    API_KEY = os.environ['OPENAI_API_KEY']
 
-prompt_template = PromptTemplate(
-    template="Give me an example of a meal that could be made using the following ingredients: {ingredients}"
-    input_variables=['ingredients']
-)
+    llm = OpenAI(openai_api_key="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", temperature=0.3)
 
-st.title("Meal Kit Planner")
-# variable with database ingredients with expiry date < {1} day
-#       different util to access these probably
-# TODO: Figure out how many ingredients put into one api request, maybe one from each category until no more can fill all categories
+    name_template = PromptTemplate(
+        template="Give me the names of two meals that could be made using the following ingredients: {ingredients}. Return them in the format of a python list of strings, and return nothing else.",
+        input_variables=['ingredients'],
+    )
 
-temp_prompt = "rice, chicken, beans"
+    meal_template = PromptTemplate(
+        template="For this meal, generate a simple cooking instruction: {meal}",
+        input_variables=['meal']
+    )
 
-if st.button("Generate") and temp_prompt: #temp
-    output = llm(prompt_template.format(ingredients=temp_prompt))
+    # st.title("Meal Kit Planner")
+    # variable with database ingredients with expiry date < {1} day
+    #       different util to access these probably
+    # TODO: Figure out how many ingredients put into one api request, maybe one from each category until no more can fill all categories
+
+    temp_prompt = "rice, chicken, beans, tomato"
+
+    meal_name_chain = LLMChain(
+        llm = llm,
+        prompt = prompt_template,
+        verbose = True,
+    )
+
+    description_chain = LLMChain(
+        llm = llm,
+        verbose=True,
+    )
+    
+    overall_chain = SequentialChain(
+        chains = [meal_name_chain, description_chain]
+        input_variables=['ingredients'],
+        output_variables=['names', 'meal_descrips'],
+        verbose=True,
+    )
+
+    output = overall_chain.run(ingredients=temp_prompt)
+    # have loading thang?
+    print(output)
+    # should get list of expired ingredients and 
+    # if st.button("Generate") and temp_prompt: #temp
+    #     output = llm(prompt_template.format(ingredients=temp_prompt))
+
+generateMealKit()
