62353671a2f514280ea05d2bcb4953450aaeefac
Saul Wolkove <saul.wolkove@gmail.com>
2024-02-03 00:30:18 -0500

    p

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -2,7 +2,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function UserData(){
     const {user, isAuthenticated, isLoading} = useAuth0();
-    console.log(user.metadata)
     if(isLoading){
         return <div>Loading...</div>
     }
