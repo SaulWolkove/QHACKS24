9f67b5b5b4596365dcad4360e4d21e780f133551
Réna Hajjar <renashajjar@gmail.com>
2024-02-03 14:22:40 -0500

    meal generation and start of object creation (with openai bad request error that i'm way too tired to fix right now

---
+++ src/components/MealKits.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+
+// export const MealKit = (oldItems) => {
+//     // should call main.py function from here....
+//     return (
+//         none,
+//     );
+// }
--- src/utils/main.py
+++ src/utils/main.py
@@ -1,16 +1,45 @@
 import os;
-from langchain_community.llms import OpenAI
+import json
+from langchain_community.llms import OpenAI as aiOpen
 from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from openai import OpenAI
 
-def generateMealKit():
 
-    # callable
+mealkit_generation_function = [
+    {
+        'name': 'extract_meal_info',
+        'description': 'Get the meal names, descriptions, and required ingredients to update database',
+        'parameters': {
+            'type': 'object',
+            'properties' : {
+                'name': {
+                    'type': 'string',
+                    'description': 'meal kit'
+                },
+                'description': {
+                    'type': 'string',
+                    'description': 'recipe description'
+                },
+                'ingredients': {
+                    'type': 'list',
+                    'description': 'ingredients required for the mealkit'
+                },
+                'username': {
+                    'type':'string',
+                    'description': 'User to which the ingredients and meal kit suggestions belong to'
+                }
+            }
+        }
+    }
+]
+
+def generateMealText():
     load_dotenv()
     API_KEY = os.environ['OPENAI_API_KEY']
 
-    llm = OpenAI(openai_api_key="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", temperature=0.3)
+    llm = aiOpen(openai_api_key="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", temperature=0.3)
 
     name_template = PromptTemplate(
         template="Give me the names of two meals that could be made using the following ingredients: {ingredients}. Return them in the format of a python list of strings, and return nothing else.",
@@ -22,8 +51,7 @@ def generateMealKit():
         input_variables=['meal']
     )
 
-    # st.title("Meal Kit Planner")
-    # variable with database ingredients with expiry date < {1} day
+    # variable with database ingredients with expiry date < {1} day *** FROM BEN'S COMPONENT ***
     #       different util to access these probably
     # TODO: Figure out how many ingredients put into one api request, maybe one from each category until no more can fill all categories
 
@@ -52,11 +80,23 @@ def generateMealKit():
 
     output = overall_chain({ 'ingredients': temp_prompt})
 
-    
-    # have loading thang?
-    print(output)
-    # should get list of expired ingredients and 
-    # if st.button("Generate") and temp_prompt: #temp
-    #     output = llm(prompt_template.format(ingredients=temp_prompt))
+    return output
 
-generateMealKit()
+def mealJSONObject():
+    load_dotenv()
+    client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+
+    meal_description = generateMealText()
+
+    print(meal_description)
+    response = client.chat.completions.create(
+        model = 'gpt-3.5-turbo',
+        functions=mealkit_generation_function,
+        function_call='auto',
+        messages = [{'role': 'user', 'content': meal_description}]
+    )
+
+    json_response = json.loads(response.choices[0].message.function_call.arguments)
+    print(json_response)
+
+mealJSONObject()
