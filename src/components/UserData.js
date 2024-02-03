859304867a1be49545fe2835ac12e59d08aef490
Saul Wolkove <saul.wolkove@gmail.com>
2024-02-02 20:14:07 -0500

    created Auth0 and cleaned up

---
+++ src/components/UserData.js
@@ -0,0 +1,19 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+function UserData(){
+    const {user, isAuthenticated, isLoading} = useAuth0();
+
+    if(isLoading){
+        return <div>Loading...</div>
+    }
+    return(
+        isAuthenticated && (
+            <div>
+                {user.name}
+            </div>
+        )
+    )
+}
+
+
+export default UserData;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Auth0Provider
+    domain='dev-15fjlo2ly7bfwmgh.us.auth0.com'
+    clientId='9dSzhQgWQ0Wx9yKa0RJ7ZhN9rSm4rWuB'
+    redirectUri={window.location.origin}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
