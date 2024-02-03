58f04bbb6a4e63b641d33f02468b4259a0295dd4
Benjamin Hilderman <hild0033@algonquinlive.com>
2024-02-03 10:29:44 -0500

    Deletebutton

--- src/components/StoreInfoLoader.js
+++ src/components/StoreInfoLoader.js
@@ -1,23 +1,43 @@
 //this is gonna load in the inventory of a store as well as any items that someone 
 //has selected
-import readItemRequest from "../api/readItemRequest";
-import {useQuery} from "react-query";
+import React from 'react';
+import './StoreInfoLoader.css';
+import { useQuery } from 'react-query';
+import readItemRequest from '../api/readItemRequest';
+import './StoreInfoLoader.css';
+import deleteItemRequest from '../api/deleteItemRequest';
 
-export default function StoreInfoLoader({username}){
-    const {isLoading, data: items} = useQuery(
+// ask if user wants to delete
+const handleDelete = async (itemId) => {
+    if (window.confirm('Are you sure you want to delete this item?')) {
+        deleteItemRequest(itemId)
+    }
+};
+
+export default function StoreInfoLoader({ username }) {
+    const { isLoading, data: items } = useQuery(
         ['items', username],
-        (username)=>readItemRequest(username)
-    )
+        () => readItemRequest(username)
+    );
 
-    return(
-        !isLoading && (
-            items.map((item)=>(
-                <div key={item._id}>
-                    {item.product}
-                    {item.expiration}
-                </div>
-            ))
-        )
-    )        
-    
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="inventory-container">
+            {items && items.length > 0 ? (
+                items.map((item) => (
+                    <div key={item._id} className="item">
+                        <div className="item-info">
+                            <p>Product: {item.product}</p>
+                            <p>Expiration: {item.expiration}</p>
+                            <button className="delete-button" onClick={() => handleDelete(item._id)}>Delete</button></div>
+                    </div>
+                ))
+            ) : (
+                <p>No items found.</p>
+            )}
+        </div>
+    );
 }
