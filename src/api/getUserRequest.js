cdf458770f309c188d191e4917c441a261d14eae
Réna Hajjar <renashajjar@gmail.com>
2024-02-04 01:34:03 -0500

    fix goof

--- src/api/getUserRequest.js
+++ src/api/getUserRequest.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-export default (arg) => {
-  return fetch(`http://localhost:3001/user/${arg}`, {
-    method: "GET",
-  }).then((response) => response.json());
-};
-=======
 export default (arg) =>{
     return fetch(`http://localhost:3001/user/${arg}`, 
     {
@@ -13,4 +6,3 @@ export default (arg) =>{
     })
     .then(response=>response.json())
 }
->>>>>>> 9246ed076cc69ba06699f0271db86c4b875062a2
