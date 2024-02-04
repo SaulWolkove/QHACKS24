ce87c119c567c79e4d0c3821d170baac4bcf464e
Saul Wolkove <saul.wolkove@gmail.com>
2024-02-04 00:12:36 -0500

    nnnnn

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-function UserData({ chooseUsername }) {
+function UserData({ setUser }) {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
@@ -9,7 +9,7 @@ function UserData({ chooseUsername }) {
 
   if (isAuthenticated) {
     // Call the chooseUsername function with user.email
-    chooseUsername(user.email);
+    setUser(user.email);
   }
 
   // You might want to return something here, even if it's null or an empty div
