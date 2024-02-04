04d0683fdcb9c6e75f6adc430423ea52379acd8d
Réna Hajjar <renashajjar@gmail.com>
2024-02-04 02:08:05 -0500

    small changes and function passes

--- src/AppTitle.js
+++ src/AppTitle.js
@@ -6,6 +6,7 @@ import StoreInfoLoader from './components/StoreInfoLoader';
 function AppTitle() {
   const [username, setUsername] = useState("");
   const [isSignUpClicked, setIsSignUpClicked] = useState(false);
+  const [accountType, setAccountType] = useState("user");
   const [showPosts, setShowPosts] = useState(true); // Changed variable name to showPosts
   const [showPostsStatus, setPostsStatus] = useState(true); // New state variable for status
 
@@ -20,7 +21,7 @@ function AppTitle() {
 
   return (
     <div className="App">
-      <TitlePage setShowPosts={handleShowPosts} setIsSignUpClicked={setIsSignUpClicked} username={username}/>
+      <TitlePage setShowPosts={handleShowPosts} setIsSignUpClicked={setIsSignUpClicked} username={username} setAccountType={setAccountType}/>
       <UserData chooseUsername={chooseUsername}/>
       <StoreInfoLoader username={username} showPostsStatus={showPostsStatus}/> {/* Pass the status as prop */}
     </div>
