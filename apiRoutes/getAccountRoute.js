2354f5a2097e6a8f3303f5f7643216be6b6c17a4
Saul Wolkove <saul.wolkove@gmail.com>
2024-02-03 22:03:20 -0500

    i

--- apiRoutes/getAccountRoute.js
+++ apiRoutes/getAccountRoute.js
@@ -2,6 +2,6 @@ const UserModel = require("../models/userModel")
 
 module.exports = async (req,res)=>{
     const username= req.query.username
-    const userData = await UserModel.find({username: username});
+    const userData = await UserModel.find({email: username});
     res.json(user);
 }
